d07e96e897a4896be3540c6bebba7149db5bf808
Tiankai-Jiang <11510109@mail.sustc.edu.cn>
2018-05-16 19:36:11 +0800

    85% finished

--- total/total.R
+++ total/total.R
@@ -18,13 +18,41 @@ for(file in tbl){
   #Find threshold, use both left or right is OK
   dat["Sum_L"] <- NA
   dat$Sum_L <- rowSums(dat[,c(2:5)])
+  total_variance <- var(dat$Sum_L)
+  
+  ###################################################################if static
+  if(total_variance<3000){
+    loop <- as.integer(nrow(dat)/300)
+    
+    #total_pressure
+    total_pressure <- mean(rowSums(dat[,c(2:9)]))
+    
+    for(b in 2:(loop-1)){
+      contact <- 300*b
+      off<- contact+100
+      a <- total_pressure
+      s = 0
+      for(i in 2:9){
+        s = s + var(dat[contact:off,i])
+      }
+      a <- c(a, s)
+      a <- c(a, substr(file, 1, nchar(file)-5))
+      res <- paste(a,collapse=",")
+      write.table(res, file="../static_parameter.csv", append = TRUE,eol ="\n", row.names=FALSE, col.names = FALSE, quote=FALSE)
+    }
+    rm(list=setdiff(ls(), "colVars"))
+    next
+  }
+  
+  ###################################################################else, it is dynamic
+  
   threshold_L <- mean(dat$Sum_L)/3
   
   #total_pressure
   total_pressure <- mean(rowSums(dat[,c(2:9)]))
   
   pair_points <- list()
-  ths = 1000
+  ths = 2000
   l = 1
   dat2 <- dat
   dat2["Ind"] <- NA
@@ -43,10 +71,6 @@ for(file in tbl){
   for(b in 2:(length(pair_points)-1)){
     contact <- pair_points[[b]][[1]]
     off<- pair_points[[b]][[2]]
-    
-    #if(length(pair_points))
-    #contact <- 588
-    #off <- 683
     
     #Order: preFC, postFC, preFO, postFO
     AnalysisWindow <- list()
@@ -130,7 +154,15 @@ for(file in tbl){
   
   #Clean up a bit
   rm(list=setdiff(ls(), "colVars"))
-  #rm(a, b, i, j, k, l, s, x, y, z, st, ed, cut, dat, dat2, off, tmp, res, ths, contact, threshold_L, Inter, AnalysisWindow)
 }
 
-rm(list=ls())
+dat_static <- read.csv("../static_parameter.csv", header = FALSE)
+static_separator <- mean(dat_static[,1])
+
+
+dat_dynamic <- read.csv("../dynamic_parameter.csv", header = FALSE)
+data <- dat_dynamic[, 1:36]
+label <- dat_dynamic[,39]
+(lda.sol = lda(data, label))
+
+rm(dat_static, dat_dynamic, colVars, data, label)
